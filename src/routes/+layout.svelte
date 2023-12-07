2a93c94013bab32b96f69fd9f58f1653ee64a8ea
TimOosterveer <62908209+TimOosterveer@users.noreply.github.com>
2023-12-07 16:49:20 +0100

    push first page transitions

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,71 @@
+<!-- <script>
+  import imaga from '$lib/assets/stek1.jpg'
+	import { fly } from 'svelte/transition';
+	import { backIn, backInOut, backOut, bounceIn, bounceInOut, cubicIn, cubicInOut, elasticOut, sineIn } from 'svelte/easing';
+	export let data;
+
+	function flush(
+		node,
+		{ delay = 0, duration = 500, easing = backInOut } = {},
+		{ direction = 'both' } = {}
+	) {
+		// const origin = {
+		//   in: 'bottom left',
+		//   out: 'bottom right',
+		//   both: 'center center'
+		// }
+
+		return {
+			delay,
+			duration,
+			easing,
+			css: (t) => `
+    scale: ${t};
+    opacity: ${t};
+    transform-origin: top center;
+    `
+		};
+	}
+
+  function loadIn(
+		node,
+		{ delay = 0, duration = 500, easing = backInOut } = {},
+		{ direction = 'both' } = {}
+	) {
+		// const origin = {
+		//   in: 'bottom left',
+		//   out: 'bottom right',
+		//   both: 'center center'
+		// }
+
+		return {
+			delay,
+			duration,
+			easing,
+			css: (t) => `
+    scale: ${t};
+    opacity: ${t};
+    transform-origin: top center;
+    `
+		};
+	}
+</script> -->
+
+<!-- {#key data.url}
+	<div in:loadIn={{ duration: 1000 }} out:flush={{ duration: 2000, delay: 500 }}>
+		<slot />
+	</div>
+{/key} -->
+
+<script>
+  import Viewtransition from "$lib/viewtransition.svelte";
+</script>
+
+
+<div class="app">
+
+  <Viewtransition />
+  <slot/>
+
+</div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let data;
+	import Home from './home/+page.svelte';
+	import NavDark from '$lib/organisms/NavDark.svelte';
+	import Footer from '$lib/organisms/Footer.svelte';
+	import MobileMenu from '$lib/organisms/MobileMenu.svelte';
+</script>
+
+
+<div>
+	
+</div>
+
+<NavDark />
+<MobileMenu />
+<Home {data} />
+
+<Footer />
+
